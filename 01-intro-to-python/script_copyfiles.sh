c716bab5470b545af33b4efe6dad34a71472b337
juan-pineda <juan.basto.pineda@gmail.com>
2019-04-08 09:47:34 -0500

    Initial commit

---
+++ 01-intro-to-python/script_copyfiles.sh
@@ -0,0 +1,26 @@
+
+DIRECTORY=~/Desktop
+if [ ! -d "$DIRECTORY" ]
+then
+  DIRECTORY=~/Escritorio
+
+  if [ ! -d "$DIRECTORY" ]
+  then
+    echo No existen los directorios ~/Desktop/ ni ~/Escritorio/
+  fi
+
+fi
+
+Taller=${DIRECTORY}/Taller_01
+
+mkdir ${Taller}
+
+cp 01_Basico.html 02_Avanzado.html ${Taller}
+cp -r images ${Taller}
+cp -r data ${Taller}
+cp ~/Anaconda3-5.1.0-Linux-x86_64.sh ${Taller}
+
+
+
+
+
